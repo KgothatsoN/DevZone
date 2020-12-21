2e125b6e6e73433e7692ace4d81066919032387c
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-21 02:12:50 +0000

    Added DB create action for saving contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,20 @@
 class ContactsController < ApplicationController
   def new
+    #Instance variable connected to new contact form
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message Sent."
+    else
+      redirect_to new_contact_path, notice: "Error!"
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
