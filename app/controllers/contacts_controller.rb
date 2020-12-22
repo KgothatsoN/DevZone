0c1036d68576919017c567e2c5ac68f9d725346e
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-22 01:53:43 +0000

    Added contacts_controller comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,16 +8,18 @@ class ContactsController < ApplicationController
     #Save contact form with details to @contact {name: 'john', email: 'email@email.com' etc}
     @contact = Contact.new(contact_params)
     if @contact.save
-      #Save and retrieve contact form input then send them to contact mailer
+      #Save Contact object to database
       name = params[:contact][:name]
       email = params[:contact][:email]
       comments = params[:contact][:comments]
+      #Send params variables to Contact Mailer
       ContactMailer.contact_email(name, email, comments).deliver
       #If all fields are filled in and saved to db then send out message
       flash[:success] = "Message Sent."
       redirect_to new_contact_path
     else
       flash[:danger] = @contact.errors.full_messages.join(", ")
+      #Redirect to new action if Contact Object fails to save
       redirect_to new_contact_path
     end
   end
