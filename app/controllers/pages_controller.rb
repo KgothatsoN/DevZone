e41d62dd01efe25f5f8aa20618999b1e8858859b
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-23 00:15:13 +0000

    Added subscription vars and params to homepage signup links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+  # GET request for / (homepage)
   def home
+    @basic_subscription = Subscription.find(1)
+    @pro_subscription = Subscription.find(2)
   end
   
+  # Get request for About page
   def about
   end
 end
