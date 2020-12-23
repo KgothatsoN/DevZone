d62a1c5aeed50ab3e6e1013aabb52821dbd0de3e
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-23 02:52:12 +0000

    Added strip customer token column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201222041618) do
+ActiveRecord::Schema.define(version: 20201223025004) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20201222041618) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
