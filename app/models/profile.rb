3572f2347a1be6c7e6c8a2343b0bf4ca13459a88
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-30 15:20:00 +0000

    Added avatar column to profile table for profile image

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :avatar,
+                    :style => { :medium => "300x300", :thumb => "100x100"},
+                    :default => "/images/:styles/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201230122153) do
+ActiveRecord::Schema.define(version: 20201230151843) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +28,12 @@ ActiveRecord::Schema.define(version: 20201230122153) do
     t.string   "telephone_number"
     t.string   "email"
     t.string   "about"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "subscriptions", force: :cascade do |t|
