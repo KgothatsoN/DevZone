eee5ecd93fdf08b694e41d1f081c49491e9bfc61
Kgothatso Ntuli <kg.otha@hotmail.com>
2020-12-30 12:42:34 +0000

    Added user profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # Render blank profile details form
-    
+    @profile = Profile.new
   end
 end
