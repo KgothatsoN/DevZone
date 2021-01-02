d4940d22851aa98c6129914083f89e97fa3b7b2d
Kgothatso Ntuli <kg.otha@hotmail.com>
2021-01-02 07:32:47 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   # Security action, lock down controller unless user is logged in
   before_action :authenticate_user!
   
+  def index
+  
+  end
   
   # GET request to /user/:id to show user profile
   def show
