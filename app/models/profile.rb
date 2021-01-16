bf20714d7eb2011914c6d93bf3661d5897e1db1b
Kgothatso Ntuli <kg.otha@hotmail.com>
2021-01-16 16:17:56 +0000

    Revert "Added default missing image for user profile. Updated spelling and grammar."
    This reverts commit 63cffa09d4c02d79c092195c2f16233a7b2f3d0d.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar,
                     :style => { :medium => "300x300", :thumb => "100x100"},
-                    :default_url => "/images/:styles/missing.png"
+                    :default => "/images/:styles/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
